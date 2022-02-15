e810c77545e016033aa68dee35f043bae7d4bf89
Yukirios <99325934+Yukirios@users.noreply.github.com>
2022-02-15 11:34:20 +0300

    added more commentaries

--- AbsSpecDecrypt.py
+++ AbsSpecDecrypt.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt #plotting
 from scipy.signal import argrelextrema #to define local extrema
 
 #reading formated excel file
+excel_file = open('test.xlsx')
 df = pd.read_excel('test.xlsx', usecols=[0,1], 
                     names= ['Wavelength(nm)', 'Absorption'])
 df.set_index('Wavelength(nm)', inplace=True)
@@ -35,7 +36,7 @@ def Maxima():
   plt.axvline(x=int(wavem), c='violet', linestyle='dashed') #vertical line of violet to the most intense
   plt.scatter(df.index, df['max'], c='g') #local maxima
   plt.plot(df.index, df['Absorption']) #smooth spectrum line
-  plt.legend(["_",'Absorption']) #auto legend
+  plt.legend(["_",'Absorption']) #auto legend, raises meme-error as it cannot add "__" to the legend, so it only provides needed
   plt.title(excel_file.name.split(".")[0])
   plt.xlabel('Wavelength(nm)')
   plt.show() #show the plot???? mb useless as sometimes shows itself
